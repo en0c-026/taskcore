4b1d8ee76b58e51d789a1811c0217113c98ac8f4
Samsung <bitup.games@gmail.com>
2023-03-15 15:05:00 +0100

    feat: add ui base

---
+++ nextjs/next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  theme: {
+    extend:
+    {
+      fontFamily: {
+        'nunito': ['Nunito', 'sans-serif']
+      },
+    },
+  },
+}
